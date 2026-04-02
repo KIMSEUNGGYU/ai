f46c123f4b73af8d0940f4831b0f59581ee7c20c
kimseunggyu <dev.seunggyu@gmail.com>
2026-04-02 09:12:20 +0900

    feat: 앱 이름 냠냠딸깍으로 변경

--- services/bab-with/src/pages/_document.tsx
+++ services/bab-with/src/pages/_document.tsx
@@ -4,13 +4,13 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
-        <meta name="application-name" content="bab-with" />
+        <meta name="application-name" content="냠냠딸깍" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta
           name="apple-mobile-web-app-status-bar-style"
           content="default"
         />
-        <meta name="apple-mobile-web-app-title" content="bab-with" />
+        <meta name="apple-mobile-web-app-title" content="냠냠딸깍" />
         <meta name="theme-color" content="#ffffff" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/icon-192.png" />
